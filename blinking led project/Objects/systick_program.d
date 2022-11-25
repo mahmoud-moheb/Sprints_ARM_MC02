29af868f777c41d76684911f6ee5e1aaea26a0f8
mahmoud moheb <mahmoud.moheb66@gmail.com>
2022-11-25 08:07:52 +0200

    commit

--- blinking led project/Objects/systick_program.d
+++
@@ -1,5 +0,0 @@
-./objects/systick_program.o: ..\COTS\MCAL\SysTick\SysTick_Program.c \
-  ..\COTS\Services\STD_TYPES.h ..\COTS\Services\BIT_MATH.h \
-  ..\COTS\MCAL\SysTick\SysTick_Private.h \
-  ..\COTS\MCAL\SysTick\SysTick_Configuration.h \
-  ..\COTS\MCAL\SysTick\SysTick_Interface.h
