89818a38f089e5d4822855841da522b78775dc1a
mahmoud moheb <mahmoud.moheb66@gmail.com>
2022-11-26 13:50:10 +0200

    final commit
    a function is added to the App layer to takes as an input from the user a specific ON time and OFF time in seconds
    push button connected to portf bin 0 to increment ontime
    push button connected to portf bin 4 to increment offtime

---
+++ blinking led project/Objects/clockcontrol_program.d
@@ -0,0 +1,6 @@
+./objects/clockcontrol_program.o: \
+  ..\COTS\MCAL\ClockControl\ClockControl_Program.c \
+  ..\COTS\MCAL\ClockControl\ClockControl_Private.h \
+  ..\COTS\MCAL\ClockControl\ClockControl_Configuration.h \
+  ..\COTS\MCAL\ClockControl\ClockControl_Interface.h \
+  ..\COTS\Services\BIT_MATH.h ..\COTS\Services\STD_TYPES.h
